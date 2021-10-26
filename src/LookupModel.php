2ff96e542edb2e2cac0c61335b56ad4b3626f797
Наранхүү Н <codesaur@gmail.com>
2021-10-26 15:17:54 +0800

    Update LookupModel.php

--- src/LookupModel.php
+++ src/LookupModel.php
@@ -17,7 +17,6 @@ class LookupModel extends MultiModel
            (new Column('id', 'bigint', 20))->auto()->primary()->unique()->notNull(),
            (new Column('keyword', 'varchar', 128))->unique(),
             new Column('category', 'varchar', 32),
-            new Column('status', 'tinyint', 1, 1),
             new Column('is_active', 'tinyint', 1, 1),
             new Column('created_at', 'datetime'),
             new Column('created_by', 'bigint', 20),
